dba9a52a503e81aa4fa377d8fdb44a9494ed1c37
Pedro Vazquez <pedrovazquezax@gmail.com>
2018-06-30 15:11:12 -0500

    dien

---
+++ weatherApp/weatherApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  weatherApp WatchKit Extension
+//
+//  Created by Pedro Antonio Vazquez Rodriguez on 29/06/18.
+//  Copyright © 2018 Pedro Antonio Vazquez Rodriguez. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Pedro Antonio Vazquez Rodriguez on 29/06/18.
+//  Copyright © 2018 Pedro Antonio Vazquez Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
