a99d49e32ef120696a1015ae8038f9b58b386348
Pedro Vazquez <pedrovazquezax@gmail.com>
2019-01-28 11:47:46 -0600

    second proyect

--- Project2/Project2 WatchKit Extension/InterfaceController.swift
+++ Project2/Project2 WatchKit Extension/InterfaceController.swift
@@ -11,11 +11,27 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet weak var result: WKInterfaceLabel!
+    @IBOutlet weak var question: WKInterfaceImage!
+    @IBOutlet weak var answers: WKInterfaceGroup!
+    @IBOutlet weak var rock: WKInterfaceButton!
+    @IBOutlet weak var papers: WKInterfaceButton!
+    @IBOutlet weak var scissors: WKInterfaceButton!
+    @IBOutlet weak var levelCounter: WKInterfaceLabel!
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    var  allMoves = ["rock","paper","scissors"]
+    var shouldWin = true
+    var level = 1
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        rock.setBackgroundImage(UIImage(named: "rock"))
+        papers.setBackgroundImage(UIImage(named: "paper"))
+        scissors.setBackgroundImage(UIImage(named: "scissors"))
+        timer.start()
+        newLevel()
     }
     
     override func willActivate() {
@@ -28,4 +44,57 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    @IBAction func rockTapped() {
+        if shouldWin{
+            check(for: "scissors")
+        }else{
+            check(for: "paper")
+        }
+    }
+    @IBAction func paperTapped() {
+        if shouldWin{
+            check(for: "rock")
+        }else{
+            check(for: "scissors")
+        }
+    }
+    @IBAction func scissorsTapped() {
+        if shouldWin{
+            check(for: "paper")
+        }else{
+            check(for: "rock")
+        }
+    }
+    
+    func  newLevel() {
+        if level == 21 {
+            result.setHidden(false)
+            question.setHidden(true)
+            answers.setHidden(true)
+            timer.stop()
+            return
+        }
+        levelCounter.setText("\(level)/20")
+        if Bool.random(){
+            setTitle("Win!")
+            shouldWin = true
+        }else {
+            setTitle("Lose!")
+            shouldWin = false
+        }
+        allMoves.shuffle()
+        question.setImage(UIImage(named: allMoves[0]))
+    }
+    func check(for answer:String) {
+        if allMoves[0] == answer{
+            level += 1
+            newLevel()
+        }
+        else{
+            level -= 1
+            if level < 1 {level = 1}
+            newLevel()
+        }
+    }
+    
 }
